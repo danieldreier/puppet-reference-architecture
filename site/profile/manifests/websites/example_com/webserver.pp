693673f4298004c88869711ca94b89c417d1e29a
Daniel Dreier <d@puppetlabs.com>
2015-02-23 21:58:45 -0800

    Add drupal-related webserver settings

--- site/profile/manifests/websites/example_com/webserver.pp
+++ site/profile/manifests/websites/example_com/webserver.pp
@@ -1,6 +1,7 @@
 class profile::websites::example_com::webserver {
   include profile::nginx
   include profile::websites::example_com::webroot
+  $website = 'example.com'
 
   $www_root = '/var/www'
 
@@ -16,18 +17,95 @@ class profile::websites::example_com::webserver {
   #  provider => 'etcd',
   #}
 
-  nginx::resource::vhost { 'example.com':
+  nginx::resource::vhost { $website:
     ensure               => present,
     rewrite_to_https     => false,
     www_root             => $www_root,
     use_default_location => false,
     index_files          => [ 'index.html' ],
     ssl                  => false,
-    server_name          => [ 'example.com', 'bar.example.com', 'www.example.com' ],
-    access_log           => '/var/log/nginx/example_com_access.log',
-    error_log            => '/var/log/nginx/example_com_error.log',
+    server_name          => [ $website, "www.${website}", "dev.${website}", "stage.${website}" ],
+    access_log           => "/var/log/nginx/${website}_access.log",
+    error_log            => "/var/log/nginx/${website}_error.log",
     notify               => Trigger['refresh load balancers'],
     require              => Class['Profile::Websites::Webroot']
+  }
+
+  nginx::resource::location { '/':
+    ensure              => 'present',
+    location_custom_cfg => {
+      try_files => '$uri @rewrite',
+    },
+    vhost               => $website,
+  }
+
+  nginx::resource::location { '~* ^/sites/default/files/(.+)$':
+    ensure              => 'present',
+    location_custom_cfg => {
+      root      => $shared_base,
+      try_files => '/files/$1 /files-backup/$1 @rewrite',
+    },
+    vhost               => $website,
+  }
+
+  cron { 'local files fallback':
+    command => "/bin/mountpoint ${shared_base}/files && /usr/bin/rsync -a ${shared_base}/files/ ${shared_base}/files-backup",
+    user    => 'root',
+    hour    => [2, 14],
+    minute  => fqdn_rand(60),
+  }
+
+  nginx::resource::location { '@rewrite':
+    ensure              => 'present',
+    location_custom_cfg => {
+      rewrite => '^ /index.php',
+    },
+    vhost               => $website,
+  }
+
+  nginx::resource::location { '~ \.php$':
+    ensure              => 'present',
+    vhost               => $website,
+    fastcgi_split_path  => '^(.+\.php)(/.+)$',
+    fastcgi_param       => {
+      'SCRIPT_FILENAME' => '$request_filename',
+    },
+    fastcgi             => "unix:${fpm_socket_file}",
+    location_custom_cfg => {
+      fastcgi_intercept_errors => 'on',
+      fastcgi_connect_timeout  => '3m',
+      fastcgi_read_timeout     => '3m',
+      fastcgi_send_timeout     => '3m',
+    },
+  }
+
+  nginx::resource::location { '/robots.txt':
+    ensure              => 'present',
+    location_custom_cfg => {
+      access_log => 'off',
+      allow      => 'all',
+    },
+    vhost               => $website,
+  }
+
+  nginx::resource::location { '~* \.(js|css|png|jpg|jpeg|gif|ico)$':
+    ensure              => 'present',
+    location_custom_cfg => {
+      expires       => 'off',
+      log_not_found => 'off',
+    },
+    vhost               => $website,
+  }
+
+  nginx::resource::location { '~ ^/(fpm-status|ping)$':
+    ensure  => 'present',
+    location_cfg_prepend => {
+      allow      => '127.0.0.1',
+      deny       => 'all',
+      access_log => 'off',
+    },
+    vhost   => $website,
+    fastcgi => "unix:${fpm_socket_file}",
   }
 
 }
